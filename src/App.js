d1bfbccaaad04cdd00ed66d7ab181d9a1ac0f91a
wxlkda <674445@pdsb.net>
2023-05-05 01:52:20 -0400

    Got a working dodeca

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Text } from '@react-three/drei';
+import './styles.css';
 
-function App() {
+const DodecahedronWithText = () => {
+  const dodecahedronRef = useRef();
+  const radius = 2;
+
+  useFrame(() => {
+    if (dodecahedronRef.current) {
+      dodecahedronRef.current.rotation.x += 0.01;
+      dodecahedronRef.current.rotation.y += 0.01;
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh ref={dodecahedronRef}>
+      <dodecahedronGeometry args={[radius]} />
+      <meshNormalMaterial />
+      {/* Add Text components for each face */}
+      <Text position={[0, 0, 0]} fontSize={1} color="black">
+        Face 1
+      </Text>
+      {/* ... */}
+    </mesh>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Canvas style={{ background: 'white' }} camera={{ position: [0, 0, 10], fov: 75 }}>
+      <ambientLight />
+      <DodecahedronWithText />
+    </Canvas>
+  );
+};
 
 export default App;
